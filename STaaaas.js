87c4ebe773aaae570f11e4bdae34b4379caef0c3
AoreTemo2626 <121981011+AoreTemo2626@users.noreply.github.com>
2023-01-10 17:11:13 +0200

    Update STaaaas.js

--- STaaaas.js
+++ STaaaas.js
@@ -1,1 +1,15 @@
-let a = 5 
+let a;
+a = Math.sqrt(25);
+
+function createFive(five){
+  let i = 1, j  = 2;
+  [i,j] = [j,i];
+  let sum = i + j + i + j - i;
+  console.log('pachti');
+  for(let u = 0; u < 5; u++){
+    sum -= u;
+    sum += u;
+  }
+  return 5}
+console.log('papapepegemabodi');
+console.log(5)
